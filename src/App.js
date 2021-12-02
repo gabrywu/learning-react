b167b2e282ae7effdc0892aaf361eac477a6f85b
unset <unset>
2021-12-02 20:46:52 +0800

    通过输入显示图形

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import {graphData} from "./graph/data"
-import {graphStyle} from "./graph/style"
+import React from "react"
+import graphDataJsonObj from "./graph/data.json"
+import graphStyleJsonObj from "./graph/style.json"
 
 // https://codesandbox.io/s/xoxhn?file=/src/App.js
 import SQLGraph from "./SQLGraph";
+import SQLTextArea from "./SQLTextArea";
 
-function App() {
-    return (
-        <SQLGraph graphData={graphData}
-                  graphStyle={graphStyle}/>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            graphDataObj: graphDataJsonObj
+        }
+        this.sqlText = JSON.stringify(graphDataJsonObj)
+    }
+
+    sqlTestUpdated(newText) {
+        this.sqlText = newText
+    }
+
+    parseSqlLineAge(e) {
+        this.setState({graphDataObj: JSON.parse(this.sqlText)})
+    }
+
+    render() {
+        return (<div>
+            <SQLTextArea value={this.state.graphDataObj} sqlTestUpdated={this.sqlTestUpdated.bind(this)}/>
+            <button className="parseButton"
+                    onClick={(e) => this.parseSqlLineAge(e)}
+            >parse
+            </button>
+            <SQLGraph graphData={this.state.graphDataObj}
+                      graphStyle={graphStyleJsonObj}/>
+        </div>)
+    }
+
 }
 
 export default App;
--- src/SQLGraph.js
+++ src/SQLGraph.js
@@ -8,18 +8,19 @@ const layout = {name: "dagre"};
 
 class SQLGraph extends React.Component {
     cytoscapeReady(cy) {
-        console.log("cy is ready " + cy)
     }
 
     render() {
         return (
-            <CytoscapeComponent elements={this.props.graphData}
-                                // https://js.cytoscape.org/#selectors
-                                stylesheet={this.props.graphStyle}
-                                style={{width: '3000px', height: '600px'}}
-                                layout={layout}
-                                autoungrabify={true}
-                                cy={this.cytoscapeReady}/>
+            <div>
+                <CytoscapeComponent elements={this.props.graphData}
+                    // https://js.cytoscape.org/#selectors
+                                    stylesheet={this.props.graphStyle}
+                                    style={{width: '1000px', height: '600px'}}
+                                    layout={layout}
+                                    cy={this.cytoscapeReady}/>
+            </div>
+
         )
     }
 }
